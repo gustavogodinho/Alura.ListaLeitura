b303c0efcbc592374f0efdaf8b89db1814a0bfc6
Gustavo Gilberto Godinho <gustavo.godinho@ituran.com.br>
2019-12-12 10:48:14 -0300

    implementacao Repository

--- Alura.ListaLeitura/Repository/BookRepositoryBase.cs
+++ Alura.ListaLeitura/Repository/BookRepositoryBase.cs
@@ -1,10 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
+using Alura.ListaLeitura.Business;
 
 namespace Alura.ListaLeitura.Repository
 {
-    class BookRepositoryBase
+    public class BookRepositoryBase : IBookRepository
     {
+        private static readonly string nomeArquivoCSV = "C:\txt\\livros.csv";
+
+        private ReadingList _paraLer;
+        private ReadingList _lendo;
+        private ReadingList _lidos;
+
+        public BookRepositoryBase()
+        {
+            var arrayParaLer = new List<Book>();
+            var arrayLendo = new List<Book>();
+            var arrayLidos = new List<Book>();
+
+            using (var file = File.OpenText(BookRepositoryBase.nomeArquivoCSV))
+            {
+                while (!file.EndOfStream)
+                {
+                    var textoLivro = file.ReadLine();
+                    if (string.IsNullOrEmpty(textoLivro))
+                    {
+                        continue;
+                    }
+                    var infoLivro = textoLivro.Split(';');
+                    var livro = new Book
+                    {
+                        Id = Convert.ToInt32(infoLivro[1]),
+                        Titulo = infoLivro[2],
+                        Autor = infoLivro[3]
+                    };
+                    switch (infoLivro[0])
+                    {
+                        case "para-ler":
+                            arrayParaLer.Add(livro);
+                            break;
+                        case "lendo":
+                            arrayLendo.Add(livro);
+                            break;
+                        case "lidos":
+                            arrayLidos.Add(livro);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            _paraLer = new ReadingList("Para Ler", arrayParaLer.ToArray());
+            _lendo = new ReadingList("Lendo", arrayLendo.ToArray());
+            _lidos = new ReadingList("Lidos", arrayLidos.ToArray());
+        }
+
+        public ReadingList ParaLer => _paraLer;
+        public ReadingList Lendo => _lendo;
+        public ReadingList Lidos => _lidos;
+
+        public IEnumerable<Book> Todos => _paraLer.Livros.Union(_lendo.Livros).Union(_lidos.Livros);
+
+        public Book BuscaLivroPorId(int id)
+        {
+            return Todos.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Incluir(Book livro)
+        {
+            var id = Todos.Select(l => l.Id).Max();
+            using (var file = File.AppendText(nomeArquivoCSV))
+            {
+                file.WriteLine($"para-ler;{id + 1};{livro.Titulo};{livro.Autor}");
+            }
+        }
     }
 }
--- Alura.ListaLeitura/Repository/IBookRepository.cs
+++ Alura.ListaLeitura/Repository/IBookRepository.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Alura.ListaLeitura.Business;
 
 namespace Alura.ListaLeitura.Repository
 {
-    interface IBookRepository
+    public interface IBookRepository
     {
+        ReadingList ParaLer { get; }
+        ReadingList Lendo { get; }
+        ReadingList Lidos { get; }
+        IEnumerable<Book> Todos { get; }
+        void Incluir(Book livro);
+
     }
 }
