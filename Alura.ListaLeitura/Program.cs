190aea177bd60dfbf16a36a1c516b9106a383682
Gustavo Gilberto Godinho <gustavo.godinho@ituran.com.br>
2019-12-12 10:00:59 -0300

    Add project files.

---
+++ Alura.ListaLeitura/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Alura.ListaLeitura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
