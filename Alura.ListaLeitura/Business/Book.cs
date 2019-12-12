df77c02a8f8d754535c9d1c6f6e64ab7a02eba80
Gustavo Gilberto Godinho <gustavo.godinho@ituran.com.br>
2019-12-12 10:35:37 -0300

    Implementacao classes Book e ReadingList

--- Alura.ListaLeitura/Business/Book.cs
+++ Alura.ListaLeitura/Business/Book.cs
@@ -1,10 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Text;
 
 namespace Alura.ListaLeitura.Business
 {
-    class Book
+    public class Book
     {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public ReadingList Lista { get; set; }
+
+        public string Details()
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("Detalhes do Livro");
+            stringBuilder.AppendLine("=====");
+            stringBuilder.AppendLine($"Título: {Titulo}");
+            stringBuilder.AppendLine($"Autor: {Autor}");
+            stringBuilder.AppendLine($"Lista: {Lista.Titulo}");
+            return stringBuilder.ToString();
+
+        }
+        public override string ToString()
+        {
+            return $"{Titulo} - {Autor}";
+        }
+
+
     }
 }
--- Alura.ListaLeitura/Business/ReadingList.cs
+++ Alura.ListaLeitura/Business/ReadingList.cs
@@ -1,10 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Alura.ListaLeitura.Business
 {
-    class ReadingList
+    public class ReadingList
     {
+        private List<Book> _livros;
+
+
+        public ReadingList(string titulo, params Book[] livros)
+        {
+            Titulo = titulo;
+            _livros = livros.ToList();
+            _livros.ForEach(l => l.Lista = this);
+        }
+
+        public string Titulo { get; set; }
+
+        public IEnumerable<Book> Livros
+        {
+            get { return _livros; }
+        }
+
+        public override string ToString()
+        {
+            var linhas = new StringBuilder();
+            linhas.AppendLine(Titulo);
+            linhas.AppendLine("=========");
+            foreach (var livro in Livros)
+            {
+                linhas.AppendLine(livro.ToString());
+            }
+            return linhas.ToString();
+        }
     }
 }
