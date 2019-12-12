5b0d4de299e168fe3dac96a02c1815b944fd28f2
Gustavo Gilberto Godinho <gustavo.godinho@ituran.com.br>
2019-12-12 10:12:07 -0300

    primeira estrutura

--- Alura.ListaLeitura/Program.cs
+++ Alura.ListaLeitura/Program.cs
@@ -2,11 +2,11 @@
 
 namespace Alura.ListaLeitura
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
