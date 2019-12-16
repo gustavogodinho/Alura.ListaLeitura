8f9a593bfbb902cec638843dbd34a92e8dc2cf2e
Gustavo Gilberto Godinho <gustavo.godinho@ituran.com.br>
2019-12-16 16:09:51 -0300

    refatoração Startup

---
+++ Alura.ListaLeitura/Services/BookServices.cs
@@ -0,0 +1,97 @@
+﻿using Alura.ListaLeitura.Business;
+using Alura.ListaLeitura.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.Services
+{
+    public class BookServices
+    {
+
+        public Task LivrosLendo(HttpContext context)
+        {
+            BookRepositoryBase _repositorio = new BookRepositoryBase();
+            return context.Response.WriteAsync(_repositorio.Lendo.ToString());
+        }
+
+        public Task LivrosLidos(HttpContext context)
+        {
+            BookRepositoryBase _repositorio = new BookRepositoryBase();
+            return context.Response.WriteAsync(_repositorio.Lidos.ToString());
+        }
+
+        public Task LivrosParaLer(HttpContext context)
+        {
+            var _repo = new BookRepositoryBase();
+
+            var conteudoArquivo = CarregaArquivoHtml("bookforRead");
+
+            foreach (var livro in _repo.ParaLer.Livros)
+            {
+                conteudoArquivo = conteudoArquivo
+                    .Replace("#NOVO-ITEM#", $"<li>{livro.Titulo} - {livro.Autor}</li>#NOVO-ITEM#");
+            }
+            conteudoArquivo = conteudoArquivo.Replace("#NOVO-ITEM#", "");
+
+            return context.Response.WriteAsync(conteudoArquivo);
+        }
+
+        public Task ExibeFormulario(HttpContext context)
+        {
+            var html = CarregaArquivoHtml("form");
+            return context.Response.WriteAsync(html);
+        }
+
+
+        public Task ExibeDetalhes(HttpContext context)
+        {
+            try
+            {
+                int id = Convert.ToInt32(context.GetRouteValue("id"));
+                BookRepositoryBase _repositorio = new BookRepositoryBase();
+                var livro = _repositorio.BuscaLivroPorId(id);
+
+                return context.Response.WriteAsync(livro.Details());
+            }
+            catch (Exception e)
+            {
+                return context.Response.WriteAsync("Não encontrado!");
+            }
+
+        }
+        public Task ProcessaFormulario(HttpContext context)
+        {
+            var livro = new Book()
+            {
+                Titulo = context.Request.Form["titulo"].First(),
+                Autor = context.Request.Form["autor"].First()
+            };
+            BookRepositoryBase _repositorio = new BookRepositoryBase();
+            _repositorio.Incluir(livro);
+
+            return context.Response.WriteAsync("Cadastrado com sucesso!");
+        }
+
+        private string CarregaArquivoHtml(string nomeArquivo)
+        {
+            try
+            {
+                var nomeCompletoArquivo = $"Views/{nomeArquivo}.html";
+                using (var arquivo = File.OpenText(nomeCompletoArquivo))
+                {
+                    return arquivo.ReadToEnd();
+                }
+            }
+            catch (Exception e)
+            {
+                return "View não encontrada!";
+            }
+
+        }
+
+    }
+}
--- Alura.ListaLeitura/Startup.cs
+++ Alura.ListaLeitura/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Alura.ListaLeitura.Business;
 using Alura.ListaLeitura.Repository;
+using Alura.ListaLeitura.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
@@ -13,76 +14,28 @@ namespace Alura.ListaLeitura
 {
     public class Startup
     {
-        BookRepositoryBase _repositorio = new BookRepositoryBase();
 
         public void Configure(IApplicationBuilder app)
         {
+            BookServices _bookServices = new BookServices();
+
             var builder = new RouteBuilder(app);
-            builder.MapRoute("Livros/ParaLer", LivrosParaLer);
-            builder.MapRoute("Livros/Lendo", LivrosLendo);
-            builder.MapRoute("Livros/Lidos", LivrosLidos);
+            builder.MapRoute("Livros/ParaLer", _bookServices.LivrosParaLer);
+            builder.MapRoute("Livros/Lendo", _bookServices.LivrosLendo);
+            builder.MapRoute("Livros/Lidos", _bookServices.LivrosLidos);
             builder.MapRoute("cadastro/novolivro/{nome}/{autor}", CadastroNovoLivro);
-            builder.MapRoute("Livros/Detalhes/{id:int}", ExibeDetalhes);
-            builder.MapRoute("cadastro/novolivro", ExibeFormulario);
-            builder.MapRoute("Cadastro/Incluir", ProcessaFormulario);
+            builder.MapRoute("Livros/Detalhes/{id:int}", _bookServices.ExibeDetalhes);
+            builder.MapRoute("cadastro/novolivro", _bookServices.ExibeFormulario);
+            builder.MapRoute("Cadastro/Incluir", _bookServices.ProcessaFormulario);
             var rotas = builder.Build();
             app.UseRouter(rotas);
         }
 
-        private string CarregaArquivoHtml(string nomeArquivo)
-        {
-            try
-            {
-                var nomeCompletoArquivo = $"Views/{nomeArquivo}.html";
-                using (var arquivo = File.OpenText(nomeCompletoArquivo))
-                {
-                    return arquivo.ReadToEnd();
-                }
-            }
-            catch (Exception e)
-            {
-                return "View não encontrada!";
-            }
-
-        }
-
-        private Task ExibeFormulario(HttpContext context)
-        {
-            var html = CarregaArquivoHtml("form");
-            return context.Response.WriteAsync(html);
-        }
-
-
-        private Task ExibeDetalhes(HttpContext context)
-        {
-            try
-            {
-                int id = Convert.ToInt32(context.GetRouteValue("id"));
-
-                var livro = _repositorio.BuscaLivroPorId(id);
-
-                return context.Response.WriteAsync(livro.Details());
-            }
-            catch (Exception e)
-            {
-                return context.Response.WriteAsync("Não encontrado!");
-            }
-
-        }
-        private Task ProcessaFormulario(HttpContext context)
-        {
-            var livro = new Book()
-            {
-                Titulo = context.Request.Form["titulo"].First(),
-                Autor = context.Request.Form["autor"].First()
-            };
-            _repositorio.Incluir(livro);
-
-            return context.Response.WriteAsync("Cadastrado com sucesso!");
-        }
+      
 
         private Task CadastroNovoLivro(HttpContext context)
         {
+            BookRepositoryBase _repositorio = new BookRepositoryBase();
             var livro = new Book
             {
                 Titulo = context.GetRouteValue("nome").ToString(),
@@ -97,31 +50,7 @@ namespace Alura.ListaLeitura
             services.AddRouting();
         }
 
-        private Task LivrosLendo(HttpContext context)
-        {
-            return context.Response.WriteAsync(_repositorio.Lendo.ToString());
-        }
-
-        private Task LivrosLidos(HttpContext context)
-        {
-            return context.Response.WriteAsync(_repositorio.Lidos.ToString());
-        }
-
-        private Task LivrosParaLer(HttpContext context)
-        {
-            var _repo = new BookRepositoryBase();
-
-            var conteudoArquivo = CarregaArquivoHtml("bookforRead");
-
-            foreach (var livro in _repo.ParaLer.Livros)
-            {
-                conteudoArquivo = conteudoArquivo
-                    .Replace("#NOVO-ITEM#", $"<li>{livro.Titulo} - {livro.Autor}</li>#NOVO-ITEM#");
-            }
-            conteudoArquivo = conteudoArquivo.Replace("#NOVO-ITEM#", "");
-
-            return context.Response.WriteAsync(conteudoArquivo);
-        }
+     
 
     }
 }
