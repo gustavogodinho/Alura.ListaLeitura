d2d767d21591608ed1a0d1a9f9efcb9676688d7b
Gustavo Gilberto Godinho <gustavo.godinho@ituran.com.br>
2019-12-17 10:04:49 -0300

    aula 08

---
+++ Alura.ListaLeitura/Controllers/BookController.cs
@@ -0,0 +1,92 @@
+﻿using Alura.ListaLeitura.Business;
+using Alura.ListaLeitura.Repository;
+using Alura.ListaLeitura.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.Controllers
+{
+    public class BookController : Controller
+    {
+        HtmlServices _htmlServices = new HtmlServices();
+
+        public string Incluir(Book book)
+        {
+            var _repositorio = new BookRepositoryBase();
+            _repositorio.Incluir(book);
+            return "Livro adicionado com sucesso!";
+        }
+
+        public string Lendo()
+        {
+            var _repositorio = new BookRepositoryBase();
+            return _repositorio.Lendo.ToString();
+        }
+
+        public string Lidos()
+        {
+            var _repositorio = new BookRepositoryBase();
+            return _repositorio.Lidos.ToString();
+        }
+
+        public string ParaLer()
+        {
+            var _repo = new BookRepositoryBase();
+            var conteudoArquivo = _htmlServices.CarregaArquivoHtml("bookforRead");
+
+            foreach (var livro in _repo.ParaLer.Livros)
+            {
+                conteudoArquivo = conteudoArquivo
+                    .Replace("#NOVO-ITEM#", $"<li>{livro.Titulo} - {livro.Autor}</li>#NOVO-ITEM#");
+            }
+
+            return  conteudoArquivo.Replace("#NOVO-ITEM#", "");
+        }
+   
+        public string ExibeFormulario()
+        {
+            var html = _htmlServices.CarregaArquivoHtml("form");
+            return html;
+        }
+
+        public string Detalhes(int id)
+        {
+            try
+            {
+                var _repositorio = new BookRepositoryBase();
+                var livro = _repositorio.BuscaLivroPorId(id);
+
+                return livro.Details();
+            }
+            catch (Exception e)
+            {
+                return "Não encontrado!";
+            }
+        }
+
+        public string ProcessaFormulario(Book book)
+        {
+            try
+            {
+                var _repositorio = new BookRepositoryBase();
+                _repositorio.Incluir(book);
+
+                return "Cadastrado com sucesso!";
+            }
+            catch (Exception e)
+            {
+                return "Erro ao cadastrar";
+            }
+        }
+
+        public string Teste()
+        {
+            return "Nova funcionalidade";
+        }
+
+
+    }
+}
--- Alura.ListaLeitura/Startup.cs
+++ Alura.ListaLeitura/Startup.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Alura.ListaLeitura.Business;
-using Alura.ListaLeitura.Repository;
-using Alura.ListaLeitura.Services;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Routing;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Alura.ListaLeitura
@@ -22,22 +13,7 @@ namespace Alura.ListaLeitura
 
         public void Configure(IApplicationBuilder app)
         {
-            //ReadController _bookServices = new ReadController();
-            //CreateController _bookCreate = new CreateController();
-
             app.UseMvcWithDefaultRoute();
-
-            //var builder = new RouteBuilder(app);
-            //builder.MapRoute("{controller}/{action}", null);
-            //builder.MapRoute("Livros/ParaLer", _bookServices.ParaLer);
-            //builder.MapRoute("Livros/Lendo", _bookServices.Lendo);
-            //builder.MapRoute("Livros/Lidos", _bookServices.Lidos);
-            //builder.MapRoute("cadastro/novolivro/{nome}/{autor}", _bookCreate.NovoLivro);
-            //builder.MapRoute("Livros/Detalhes/{id:int}", _bookServices.Detalhes);
-            //builder.MapRoute("cadastro/ExibeFormulario", _bookServices.ExibeFormulario);
-            //builder.MapRoute("Cadastro/Incluir", _bookServices.Incluir);
-            //var rotas = builder.Build();
-            //app.UseRouter(rotas);
         }
 
        
